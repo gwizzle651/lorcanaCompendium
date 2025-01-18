e8cead4d27e2b8e532dbbb135004c31d499176d3
gwizzle651 <gwizzle651@proton.me>
2025-01-17 21:26:49 -0600

    setup

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+//       |\      _,,,---,,_
+// ZZZzz /,`.-'`'    -.  ;-;;,_
+//      |,4-  ) )-,_. ,\ (  `'-'
+//     '---''(_/--'  `-'\_)  Written by: gwizzle651
+#include <iostream>
+
+
+int main(){
+    std::cout << "hello world";
+    return 0;
+}
